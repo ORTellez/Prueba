317e27c98bdb1320de8d255717c1f1c9533bbc14
hola <hola@Octavio>
2015-04-09 14:45:39 -0700

    Funciona?

---
+++ Prueba/src/Logic/ClasePrueba.java
@@ -0,0 +1,14 @@
+package Logic;
+
+public class ClasePrueba {
+
+	public ClasePrueba() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Este es un comentario para ver si funciona el github xP
+	}
+
+}
